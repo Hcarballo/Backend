b74540734a534ff584101314a089f38ad88267df
hernanc77 <hernancarballo@hotmail.com>
2023-11-20 21:22:55 -0300

    Backend_Desafio_1

---
+++ ProductManager.js
@@ -0,0 +1,53 @@
+class ProductManager {
+    products;
+    static lastId = 0;
+
+   
+
+    constructor() {
+        this.products = [];
+        console.log("HOLA")
+    }
+
+    
+    getProducts() {
+        return this.products;
+    }
+
+    addProduct(title, description, price, code, stock) {
+        ProductManager.lastId++;
+        const product = {
+            id: ProductManager.lastId,
+            description,
+            price,            
+            thumbnail, 
+            code,           
+            stock
+        };
+        
+        if (!product.code || !product.description || !product.price || !product.thumbnail || product.stock) {
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+
+        for (let i = 0; i < this.products.length; i++) {
+            if (this.products[i].codigo === product.codigo) {
+                console.log("El código del producto ya existe");
+                return;
+            }
+        }
+                
+        this.products.push(product);
+    }
+
+    getProductById(id) {
+        for (let i = 0; i < this.products.length; i++) {
+            if (this.products[i].id === id) {
+                return this.products[i];
+            }
+        }
+
+        console.log("No se encontró el producto");
+    }
+
+}
