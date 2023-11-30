575d1f14de2333e372a8e4a3b14779756d8d540c
hernanc77 <hernancarballo@hotmail.com>
2023-11-29 21:33:25 -0300

    Desafio 2 v0.1

--- ProductManager.js
+++ ProductManager.js
@@ -1,20 +1,39 @@
+const fs = require('fs');
+const { get } = require('http');
+
 class ProductManager {
     products;
-    static lastId = 0;
+    path;
 
     constructor() {
         this.products = [];
+        this.path = "./product.txt"
     }
 
-
     getProducts() {
-        return this.products;
+        if (fs.existsSync(this.path)) {
+            const productsData = fs.readFileSync(this.path, "utf-8");
+            this.products = productsData.split('\n').filter(line => line).map(line => JSON.parse(line));
+            return this.products;
+        }
+    }
+
+    getLastIdProducts() {
+        const products = this.getProducts();
+        let id = 0;
+        if (products.length > 0) {
+            id = Math.max(...products.map(p => p.id));
+            id++;
+            console.log(id);
+        }
+
+        return id;
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
-        ProductManager.lastId++;
+
         const product = {
-            id: ProductManager.lastId,
+            id: this.getLastIdProducts(),
             title,
             description,
             price,
@@ -25,34 +44,72 @@ class ProductManager {
 
         if (!product.code || !product.description || !product.price || !product.thumbnail || !product.stock) {
             console.log("Todos los campos son obligatorios");
-            ProductManager.lastId--;
             return;
         }
 
         if (this.products.some(p => p.code === product.code)) {
             console.log("El código del producto ya existe");
-            ProductManager.lastId--;
             return;
         }
 
         this.products.push(product);
+        fs.appendFileSync(this.path, JSON.stringify(product) + '\n');
     }
 
     getProductById(id) {
-        if (this.products.length > 0) {
-            return this.products.find(id)
+        const products = this.getProducts();
+        const product = products.find(p => p.id === id);
+
+        if (product != undefined) {
+            return product;
         }
 
         console.log("No se encontró el producto");
-    }  
+    }
 
+    upDateProduct(id, updateAtrib) {
+        const products = this.getProducts();
+        const productIndex = products.findIndex(p => p.id === id);
+
+        if (productIndex === -1) {
+            console.log("No se encontró el producto");
+            return;
+        }
+
+        products[productIndex] = { ...products[productIndex], ...updateAtrib };
+
+        const productsData = products.map(product => JSON.stringify(product)).join('\n');
+        fs.writeFileSync(this.path, productsData);
+    }
+
+    deleteProduct(id) {
+        const products = this.getProducts();
+        const productIndex = products.findIndex(p => p.id === id);
+
+        if (productIndex === -1) {
+            console.log("No se encontró el producto");
+            return;
+        }
+
+        products.splice(productIndex, 1);
+
+        const productsData = products.map(product => JSON.stringify(product)).join('\n');
+        fs.writeFileSync(this.path, productsData);
+    }
 
 }
 
+//------------Testing---------------
+
 const productManager = new ProductManager();
 
-productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc127", 10); // Id = 1
-productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc127", 10); // Código repetido
-productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc128", 10); // Id = ?
+productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc129", 10);
+productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc130", 10);
+productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc130", 10); // Código repetido
+productManager.addProduct("producto prueba", "es la descrip de prueba", 100, "sin imagen", "abc131", 10);
 
 console.log(productManager.getProducts());
+console.log(productManager.getProductById(2));
+
+productManager.upDateProduct(4, { title: "Cambio/Actualizacion", price: 200 })
+productManager.deleteProduct(3)
